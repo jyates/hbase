57e4928249938c811855bdbd2663bcdce5359e2c
Matteo Bertozzi <matteo.bertozzi@cloudera.com>
2012-10-18 21:27:51 +0200

    Use FSVisitor for ReferenceRegionHFileTask

--- hbase-server/src/main/java/org/apache/hadoop/hbase/server/snapshot/task/ReferenceRegionHFilesTask.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/server/snapshot/task/ReferenceRegionHFilesTask.java
@@ -18,7 +18,8 @@
 package org.apache.hadoop.hbase.server.snapshot.task;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.TreeMap;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -26,10 +27,13 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.PathFilter;
+import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.io.Reference;
 import org.apache.hadoop.hbase.protobuf.generated.HBaseProtos.SnapshotDescription;
 import org.apache.hadoop.hbase.server.snapshot.TakeSnapshotUtils;
 import org.apache.hadoop.hbase.server.snapshot.error.SnapshotErrorListener;
 import org.apache.hadoop.hbase.util.FSUtils;
+import org.apache.hadoop.hbase.util.FSVisitor;
 
 /**
  * Reference all the hfiles in a region for a snapshot.
@@ -42,7 +46,6 @@ public class ReferenceRegionHFilesTask extends SnapshotTask {
   public static final Log LOG = LogFactory.getLog(ReferenceRegionHFilesTask.class);
   private final Path regiondir;
   private final FileSystem fs;
-  private final PathFilter fileFilter;
   private final Path snapshotDir;
 
   /**
@@ -58,62 +61,32 @@ public class ReferenceRegionHFilesTask extends SnapshotTask {
     super(snapshot, monitor, "Reference hfiles for region:" + regionDir.getName());
     this.regiondir = regionDir;
     this.fs = fs;
-
-    this.fileFilter = new PathFilter() {
-      @Override
-      public boolean accept(Path path) {
-        try {
-          return fs.isFile(path);
-        } catch (IOException e) {
-          LOG.error("Failed to reach fs to check file:" + path + ", marking as not file");
-          ReferenceRegionHFilesTask.this.snapshotFailure("Failed to reach fs to check file status",
-            e);
-          return false;
-        }
-      }
-    };
     this.snapshotDir = regionSnapshotDir;
   }
 
   @Override
   public void process() throws IOException {
-    FileStatus[] families = FSUtils.listStatus(fs, regiondir, new FSUtils.FamilyDirFilter(fs));
+    FSVisitor.regionStoreFiles(fs, regiondir, new FSVisitor.StoreFileVisitor() {
+      private final Map<String, Path> snapshotFamilyDirs = new TreeMap<String, Path>();
 
-    // if no families, then we are done again
-    if (families == null || families.length == 0) {
-      LOG.info("No families under region directory:" + regiondir
-          + ", not attempting to add references.");
-      return;
-    }
-
-    // snapshot directories to store the hfile reference
-    List<Path> snapshotFamilyDirs = TakeSnapshotUtils.getFamilySnapshotDirectories(snapshot,
-      snapshotDir, families);
-
-    LOG.debug("Add hfile references to snapshot directories:" + snapshotFamilyDirs);
-    for (int i = 0; i < families.length; i++) {
-      FileStatus family = families[i];
-      Path familyDir = family.getPath();
-      // get all the hfiles in the family
-      FileStatus[] hfiles = FSUtils.listStatus(fs, familyDir, fileFilter);
-
-      // if no hfiles, then we are done with this family
-      if (hfiles == null || hfiles.length == 0) {
-        LOG.debug("Not hfiles found for family: " + familyDir + ", skipping.");
-        continue;
-      }
-
-      // make the snapshot's family directory
-      Path snapshotFamilyDir = snapshotFamilyDirs.get(i);
-      fs.mkdirs(snapshotFamilyDir);
-
-      // create a reference for each hfile
-      for (FileStatus hfile : hfiles) {
-        Path referenceFile = new Path(snapshotFamilyDir, hfile.getPath().getName());
-        LOG.debug("Creating reference for:" + hfile.getPath() + " at " + referenceFile);
+      public void storeFile(final String regionInfo, final String family,
+          final String hfileName) throws IOException {
+        Path referenceFile = getSnapshotRefFile(family, hfileName);
+        LOG.debug("Creating reference for: " + hfileName + " at " + referenceFile);
         FSUtils.touch(fs, referenceFile);
       }
-    }
+
+      private Path getSnapshotRefFile(final String family, final String fileName) throws IOException {
+        Path familyDir = this.snapshotFamilyDirs.get(family);
+        if (familyDir == null) {
+          familyDir = TakeSnapshotUtils.getStoreSnapshotDirectory(snapshotDir, family);
+          fs.mkdirs(familyDir);
+          snapshotFamilyDirs.put(family, familyDir);
+        }
+        return new Path(familyDir, fileName);
+      }
+    });
+
     if (LOG.isDebugEnabled()) {
       LOG.debug("Finished referencing hfiles, current fs state:");
       FSUtils.logFileSystemState(fs, fs.getWorkingDirectory(), LOG);
